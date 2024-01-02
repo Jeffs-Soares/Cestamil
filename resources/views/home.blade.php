92fdec03fdd3c16e8c9af1cf4b615a38015cd6f8
Jeffs-Soares <soares.jeffersonthadeu@gmail.com>
2024-01-02 17:55:23 -0300

    refactor: small changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,11 +5,11 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Login Page </title>
+    <title>Home Page </title>
 </head>
 <body>
 
-<h1> Login Page </h1>
+<h1> Home Page </h1>
 
 
 
