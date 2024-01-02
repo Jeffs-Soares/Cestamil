5474dde354ab84827b5f6494d4ed6ff0a45875ab
Jeffs-Soares <soares.jeffersonthadeu@gmail.com>
2024-01-02 11:16:59 -0300

    refactor: small changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,5 +13,13 @@
 
 
 
+<a href="{{ route('budget.index')}}">Budgets</a>
+<br>
+<br>
+<a href="{{ route('region.index')}}">Regions</a>
+<br>
+<br>
+<a href="{{ route('product.index')}}">Products</a>
+
 </body>
 </html>
