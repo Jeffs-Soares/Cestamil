493c783ba8c5c8b32cfca0131e1c488334d895dc
Jeffs-Soares <soares.jeffersonthadeu@gmail.com>
2024-03-11 12:19:57 -0300

    product logic validation

--- resources/views/product/create.blade.php
+++ resources/views/product/create.blade.php
@@ -26,14 +26,12 @@
 
         @if($errors->any())
             <div>
-                <ul class="flex flex-col">
+                <ul>
                     @foreach($errors->all() as $error)
-                        <li class="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-500 dark:text-red-400 font-medium"> {{$error }}</li>
+                        <li > {{$error }}</li>
                     @endforeach
                 </ul>
             </div>
-
-
         @endif
 
 
