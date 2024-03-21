3f831ec2ed0ef77766c1b8971432082b436a49d5
Jeffs-Soares <soares.jeffersonthadeu@gmail.com>
2024-03-21 11:36:34 -0300

    making dashboard with flowbite

--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,6 @@
 use App\Http\Controllers\{BudgetController, ProductController, RegionController};
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('login');
 });
@@ -27,3 +16,7 @@ Route::resource('budget', BudgetController::class);
 Route::get('/budget/{budget}/pay', [BudgetController::class, 'payCreate'])->name('payCreate');
 
 Route::put('/budget/{budget}/pay', [BudgetController::class, 'payStore'])->name('payStore');
+
+Route::get('/dashboard', function () {
+    return view('welcome');
+});
