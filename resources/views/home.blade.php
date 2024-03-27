dfe0de5b1f1a5233cd9a2fc3e916ccc1a90e9244
Jeffs-Soares <soares.jeffersonthadeu@gmail.com>
2024-03-27 12:20:01 -0300

    working in home dashboard page content

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,41 @@
 <x-app.template>
 
+
         <h1 class="text-center text-4xl py-14"> Home </h1>
+
+
+    <div class="grid grid-cols-3 gap-2">
+
+
+    <a href="" class="block w-max">
+        <div class="p-2 px-4 h-[200px] w-[200px] bg-gray-200 rounded-lg dark:bg-gray-800 flex flex-col gap-6">
+            <h2 class="text-gray-50 text-center"> Orçamentos Totalmente Pagos </h2>
+
+            <p class="text-center text-gray-50 text-5xl"> 5 </p>
+
+        </div>
+    </a>
+
+    <a href="" class="block w-max">
+        <div class="p-2 px-4 h-[200px] w-[200px] bg-gray-200 rounded-lg dark:bg-gray-800 flex flex-col gap-6">
+            <h2 class="text-gray-50 text-center"> Orçamentos Parcialmente Pagos </h2>
+
+            <p class="text-center text-gray-50 text-5xl"> 2 </p>
+
+        </div>
+    </a>
+
+    <a href="" class="block w-max">
+        <div class="p-2 px-4 h-[200px] w-[200px] bg-gray-200 rounded-lg dark:bg-gray-800 flex flex-col gap-6">
+            <h2 class="text-gray-50 text-center"> Orçamentos não pagos </h2>
+
+            <p class="text-center text-gray-50 text-5xl "> 10 </p>
+
+        </div>
+    </a>
+
+    </div>
+
 
 </x-app.template>
 
